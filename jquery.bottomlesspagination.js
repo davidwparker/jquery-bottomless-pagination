87ee9f12eae313ed9e5d335e744b842024e58c2f
David Morrison <dmorrison@junointeractive.com.au>
2011-07-05 17:36:50 -0700

    Enable loading message or spinner Setup loader when loaded Get total_pages count and display end of list when reached Added icon span to More link

--- jquery.bottomlesspagination.js
+++ jquery.bottomlesspagination.js
@@ -8,57 +8,72 @@
       objName:'',
       callback:null
     },callerSettings||{});
+    
     settings.imgLoader = new Image();
     settings.imgLoader.src = settings.ajaxLoaderPath;
-    settings.href = $(".current").next().attr("href");
+    settings.href = $(".next_page").attr("href");
+    
+    // Get the total number of pages from the last numbered pagination link
+    last_pagination_link = $(".pagination a").not(".next_page, .previous_page").last();
+    settings.total_pages = getPageNumber(last_pagination_link.attr('href'));
   	
+  	// If pagination exists, hide existing and append the bottomless pagination html
     if ($('div.pagination').size() > 0){
       $('div.pagination').wrap("<div class='pagination_links'></div>").hide();
+      
       $('.pagination_links').append(
-        "<div class='live_pagination'>" +
-          "<a class='more_links' style='cursor:pointer;'>Show more " + settings.objName + "...</a>" + 
-        "</div>"
+        "<div class='clearfix live_pagination'><div>" +
+          "<a class='more_links'>More " + settings.objName + "<span class='icon'></span></a>" +
+        "</div></div>"
       );
+      
+      // Setup the loader. If no image provided use a text message. Loader is initially hidden.
+      if ((settings.ajaxLoaderPath == '') || (settings.ajaxLoaderPath == null)) {
+        $(".more_links").after("<div class='now_loading'>Loading...</div>");
+      } else {
+        $(".more_links").after("<div class='now_loading'><img src='"+settings.imgLoader.src+"' /></div>");
+      }
+      
+      $(".now_loading").hide();
     }
-    $(".more_links").click(function(){
+    
+    $(".more_links").click(function(e){
+      e.preventDefault();
       $(".more_links").hide();
-      if ($(".now_loading").size() == 0)
-        $(".more_links").after("<img class='now_loading' src='"+settings.imgLoader.src+"' />");
-      else
-        $(".now_loading").show();
-      $.get(
-        settings.href,'',function(data){
+      $(".now_loading").show();
+      
+      // Get the data & update links
+      $.get(settings.href,function(data){
           $(settings.results).addrows(data);
+          $(".now_loading").hide();
+          $(".more_links").show();
         }
       );
-      $(".now_loading").hide();
-      $(".more_links").show();
       return false;
     });
     
     $.fn.addrows = function(data) {
-      //remove live pagination if there are no more jobs
-      if (data.length === 0 ){
-        $('.live_pagination').remove();
-        $('.pagination_links').append(
-          "<div class='no_pagination'>" +
-            "There are no more " + settings.objName + " to add..." + 
-          "</div>"
-        );
-        return false;
-      }
-
-      var pageRE     = /page=(\d+)/,
-          pageMatch  = settings.href.match(pageRE);
-
-      if (pageMatch) {
-        var pageNumber  = parseInt(pageMatch[1]) + 1;
-        settings.href = settings.href.replace(pageRE, 'page=' + pageNumber);
-      }
-
-      //add results to the page
+      curr_page = getPageNumber(settings.href);
       $(settings.results).append(data);
+      
+      // Replace live pagination if there are no more results, else update the href for the next page
+      if (curr_page == settings.total_pages) {
+        $('.live_pagination').remove();
+        $('.pagination_links').append('<div class="live_pagination back_to_top"><a class="top">Back to top &uarr;</a></div>');
+        return false;
+      } else {
+        next_page = curr_page + 1;
+        settings.href = settings.href.replace('page=' + curr_page, 'page=' + next_page.toString());
+      }
+      
       if (settings.callback) settings.callback();
     };
+    
+    function getPageNumber(url) {
+      pageRE = /page=(\d+)/;
+      pageMatch = url.match(pageRE);
+      return parseInt(pageMatch[1]);
+    };
+    
   };
 })(jQuery);
